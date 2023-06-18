1c4dc71c452c1a496d84d72912388843d99edcf9
bwarren3 <bwarren3@cs6261-vm11.uwg.westga.edu>
2023-06-18 17:02:12 -0400

    Completed Part 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HelloComponent } from './tournament/hello/hello.component';
+import { RegistrationComponent } from './tournament/registration/registration.component';
+import { BracketsComponent } from './tournament/brackets/brackets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'hello', component: HelloComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'brackets', component: BracketsComponent },
+  { path: "**", component: HelloComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
